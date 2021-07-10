a8c3ec67f45e72247440eae5890802d628eeb0af
Ghie Cotcheza <ghie.cotcheza@gmail.com>
2021-07-10 20:13:20 +0200

    route error:unexpected identifier \"Route\"

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -10,16 +10,15 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends BaseController
 {
     public function signin(Request $request)
-    {  
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
-            $authUser = Auth::user(); 
-            $success['token'] =  $authUser->createToken('surveyBuilderAppToken')->plainTextToken; 
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $authUser = Auth::user();
+            $success['token'] =  $authUser->createToken('surveyBuilderAppToken')->plainTextToken;
             $success['name'] =  $authUser->name;
-   
+
             return $this->sendResponse($success, 'User signed in');
-        } 
-        else{ 
-            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
-        } 
+        } else {
+            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
+        }
     }
 }
